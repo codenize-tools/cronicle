5c8ba4228be8958bda2eab11514cf7cf98e48e6d
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-01-25 13:49:50 +0900

    Add Hash ext

--- lib/cronicle.rb
+++ lib/cronicle.rb
@@ -2,5 +2,6 @@ require 'sshkit'
 require 'sshkit/dsl'
 
 module Cronicle; end
+require 'cronicle/ext/hash_ext'
 require 'cronicle/dsl'
 require 'cronicle/version'
---
+++ lib/cronicle/ext/hash_ext.rb
@@ -0,0 +1,8 @@
+class Hash
+  def assert_valid_keys(*valid_keys)
+    valid_keys.each do |k|
+      next if valid_keys.include?(k)
+      raise ArgumentError, "Unknown key: #{k.inspect}. Valid keys are: #{valid_keys.map(&:inspect).join(', ')}"
+    end
+  end
+end
