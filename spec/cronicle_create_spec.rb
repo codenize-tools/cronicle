ed5a45dd08f8d727e0474e469c39939106f1f4ee
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-02-08 21:08:57 +0900

    Fix create spec

--- spec/cronicle_create_spec.rb
+++ spec/cronicle_create_spec.rb
@@ -1,5 +1,108 @@
 describe 'Cronicle#apply (create)' do
   context 'when empty cron' do
+    let(:jobfile) do
+      <<-RUBY.undent
+        on servers: /.*/ do
+          job :foo, user: :root, schedule: '1 2 * * *' do
+            puts `uname`
+            puts `whoami`
+          end
+        end
+
+        on servers: /.*/ do
+          job :bar, user: :root, schedule: :@hourly, content: <<-SH.undent
+            #!/bin/sh
+            echo hello
+          SH
+        end
+
+        on servers: /amazon_linux/ do
+          job :foo, user: 'ec2-user', schedule: '1 * * * *' do
+            puts 100
+          end
+        end
+
+        on servers: /ubuntu/ do
+          job :foo, user: :ubuntu, schedule: :@daily do
+            puts 200
+          end
+        end
+      RUBY
+    end
+
+    let(:amzn_crontab) do
+      {
+        "/var/spool/cron/ec2-user" =>
+"1 * * * *\t/var/lib/cronicle/libexec/ec2-user/foo 2>&1 | logger -t cronicle/ec2-user/foo
+",
+        "/var/spool/cron/root" =>
+"1 2 * * *\t/var/lib/cronicle/libexec/root/foo 2>&1 | logger -t cronicle/root/foo
+@hourly\t/var/lib/cronicle/libexec/root/bar 2>&1 | logger -t cronicle/root/bar
+"
+      }
+    end
+
+    let(:ubuntu_crontab) do
+      {
+        "/var/spool/cron/crontabs/root" =>
+"1 2 * * *\t/var/lib/cronicle/libexec/root/foo 2>&1 | logger -t cronicle/root/foo
+@hourly\t/var/lib/cronicle/libexec/root/bar 2>&1 | logger -t cronicle/root/bar
+",
+        "/var/spool/cron/crontabs/ubuntu" =>
+"@daily\t/var/lib/cronicle/libexec/ubuntu/foo 2>&1 | logger -t cronicle/ubuntu/foo
+"
+      }
+    end
+
+    before do
+      cronicle(:apply) { jobfile }
+    end
+
+    it do
+      on :amazon_linux do
+        expect(get_uname).to match /amzn/
+        expect(get_crontabs).to eq amzn_crontab
+
+        expect(get_file('/var/lib/cronicle/libexec/root/foo')).to eq <<-EOS.undent
+          #!/usr/bin/env ruby
+          puts `uname`
+          puts `whoami`
+        EOS
+
+        expect(get_file('/var/lib/cronicle/libexec/root/bar')).to eq <<-EOS.undent
+          #!/bin/sh
+          echo hello
+        EOS
+
+        expect(get_file('/var/lib/cronicle/libexec/ec2-user/foo')).to eq <<-EOS.undent
+          #!/usr/bin/env ruby
+          puts 100
+        EOS
+      end
+    end
+
+    it do
+      on :ubuntu do
+        expect(get_uname).to match /Ubuntu/
+        expect(get_crontabs).to eq ubuntu_crontab
+
+        expect(get_file('/var/lib/cronicle/libexec/root/foo')).to eq <<-EOS.undent
+          #!/usr/bin/env ruby
+          puts `uname`
+          puts `whoami`
+        EOS
+
+        expect(get_file('/var/lib/cronicle/libexec/root/bar')).to eq <<-EOS.undent
+          #!/bin/sh
+          echo hello
+        EOS
+
+        expect(get_file('/var/lib/cronicle/libexec/ubuntu/foo')).to eq <<-EOS.undent
+          #!/usr/bin/env ruby
+          puts 200
+        EOS
+      end
+    end
   end
 
   context 'when default cron' do
