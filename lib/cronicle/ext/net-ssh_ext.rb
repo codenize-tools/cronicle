dbf22a08ca609728f723aa511a0ee2479a70a88a
Genki Sugawara <sugawara@cookpad.com>
2015-02-10 19:07:38 +0900

    Fix prompt check

--- lib/cronicle/ext/net-ssh_ext.rb
+++ lib/cronicle/ext/net-ssh_ext.rb
@@ -1,11 +1,13 @@
 class Net::SSH::Connection::Channel
+  PROMPT_REGEXP = Regexp.compile('^' + Regexp.escape(SSHKit::Backend::Netssh::SUDO_PROMPT) + '\b')
+
   alias on_data_orig on_data
 
   def on_data(&block)
     on_data_orig do |ch, data|
       sudo_password = Thread.current[SSHKit::Backend::Netssh::SUDO_PASSWORD_KEY]
 
-      if sudo_password and data == SSHKit::Backend::Netssh::SUDO_PROMPT
+      if sudo_password and data =~ PROMPT_REGEXP
         ch.send_data(sudo_password + "\n")
       else
         block.call(ch, data) if block
